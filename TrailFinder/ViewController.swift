4bdcd0f4146477691846a828c875d1867365c9d6
Amanda Harman <aharman@GNVaharman3SDV33.local>
2016-06-03 18:35:29 -0400

    Set up basic tableView with fake data

--- TrailFinder/ViewController.swift
+++ TrailFinder/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let trail1 = Trail(name: "Hawthorn", parkName: "Gainesville", description: "A good one")
+    let trail2 = Trail(name: "Amanda's Trail", parkName: "Panda Forest", description: "This one's for PB")
+    
+    var trails = [Trail]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        trails += [trail1, trail2]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +33,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("trailCell") as! TrailTableViewCell
+    
+        cell.titleLabel.text = trails[indexPath.row].name
+        cell.descriptionLabel.text = trails[indexPath.row].description
+        cell.parkLabel.text = trails[indexPath.row].parkName
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trails.count
+    }
 
 }
 
